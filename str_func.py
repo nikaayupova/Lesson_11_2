c23862598e8697da1a593af30af57d6ed87be0c6
Nika Ayupova <ayupova.nik@yandex.ru>
2023-06-11 13:25:08 +0300

    change str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,20 @@
 def foo(word):
+    """
+
+    :param word:
+    :return:
+    """
     return word.upper()
+
+
+def foo_2(word):
+    """
+
+    :param word:
+    :return:
+    """
+    words_list = word.split()
+    result = []
+    for word in words_list:
+        result.append(word.capitalize())
+    return ' '.join(result)
